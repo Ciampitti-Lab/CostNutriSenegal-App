085d9afe5cee21ad84a7a48194b1a6f16cdc3f87
Gustavo N. Santiago <104911112+GustavoSantiago113@users.noreply.github.com>
2023-02-21 17:01:43 -0600

    Version to feedbacks

--- functions.R
+++ functions.R
@@ -1,14 +1,19 @@
 # Libraries load ---- 
 library(dplyr)
-library(tidyverse)
+library(readxl)
+
+dataFCards <- read.csv("data/Alternative_Scenarios_Card.csv", sep=",", dec=".")
 
 # Card Filtering ----
 card_filtering <- function(dataBase, district, baseline, pd, pp, n, acronym, scenario){
+  
+  district <- toString(district)
   
   if(baseline == TRUE){
     identification = "baseline"
   }
   else{
+    pp <- as.integer(pp)
     identification = paste0("pd", pd, "pp", pp, "n", n)
   }
   
@@ -25,8 +30,38 @@ card_filtering <- function(dataBase, district, baseline, pd, pp, n, acronym, sce
 }
 
 # Graph Filtering ----
-graph_filtering <- function(dataBase, district, baseline, pd, pp, n, acronym, scenario){
+graph_filtering <- function(district, baseline, pd, pp, n, acronym){
   
+  path <- paste("data/Data_for_Kansas_", district, ".xlsx", sep = "")
   
+  if(baseline == TRUE){
+    sheet = "Baseline"
+    column1 = as.name(str_glue(acronym, " Base ", "1"))
+    column2 = as.name(str_glue(acronym, " Base ", "5"))
+  }
+  else{
+    sheet = paste0("Alt ", pd, " ", pp, " ", n, "kg")
+    column1 = as.name(str_glue(acronym, " Alt ", "1"))
+    column2 = as.name(str_glue(acronym, " Alt ", "5"))
+  }
+  
+  dataBase <- read_excel(path=path, sheet = sheet)
+
+  values <- dataBase %>%
+    select(column1:column2)
+  
+  d1 <- data.frame(data=unlist(values, use.names = FALSE))
+  
+  Scenarios <- rep(c("Alt 1", "Alt 2", "Alt 3", "Alt 4", "Alt 5"),each=500)
+  d2 <- cbind(d1, Scenarios)
+  
+  return(d2)
   
 }
+
+dataBase <- graph_filtering(district = "THIES",
+                            baseline = FALSE,
+                            pd = 1,
+                            pp = 3.3,
+                            n = 20,
+                            acronym = "KCal")
