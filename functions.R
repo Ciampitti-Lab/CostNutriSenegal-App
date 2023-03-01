08ff267b85a3833a21ce8282c1b59bd7469740c9
Gustavo N. Santiago <104911112+GustavoSantiago113@users.noreply.github.com>
2023-03-01 17:06:18 -0600

    Michigan's feedback pt1

--- functions.R
+++ functions.R
@@ -5,13 +5,52 @@ library(stringr)
 
 dataFCards <- read.csv("data/Alternative_Scenarios_Card.csv", sep=",", dec=".")
 
+# 1.0 Map Rendering ----
+map_rendering <- function(districtDB, district){
+  
+  districtSelected <- districtDB %>%
+    dplyr::filter(DISTRICT == district)
+  
+  map <- leaflet() %>%
+    addTiles() %>%
+    addPolygons( data = districtDB,
+                 weight = 1,
+                 smoothFactor = 0.2,
+                 fillOpacity = 0.2,
+                 color = "black",
+                 highlight = highlightOptions(
+                   weight = 5,
+                   color = "#666666",
+                   fillOpacity = 0.25,
+                   bringToFront = TRUE
+                 ),
+                 label = districtDB$DISTRICT
+    ) %>%
+    addPolygons( data = districtSelected,
+                 weight = 1,
+                 smoothFactor = 1,
+                 fillOpacity = 1,
+                 color = "#1c5f7e",
+                 highlight = highlightOptions(
+                   weight = 5,
+                   color = "#666666",
+                   fillOpacity = 0.5,
+                   bringToFront = TRUE
+                 )
+    )
+  
+  return(map)
+  
+}
+
 # Card Filtering ----
-card_filtering <- function(dataBase, district, baseline, pd, pp, n, acronym, scenario){
+card_filtering <- function(dataBase, district, precipitation, pd, pp, n, acronym, year){
   
   district <- toString(district)
   
-  if(baseline == TRUE){
-    identification = "baseline"
+  if(precipitation == TRUE){
+    identification = "precipitation"
+    ## Add something after precipitation table arrive
   }
   else{
     pp <- as.integer(pp)
@@ -22,7 +61,7 @@ card_filtering <- function(dataBase, district, baseline, pd, pp, n, acronym, sce
     dplyr::filter(id == identification,
                   District == district)
   
-  variable = as.name(str_glue(acronym, ".Alt.", scenario))
+  variable = as.name(str_glue(acronym, ".Alt.", year))
   
   value = dataBase[[variable]]
   
@@ -30,47 +69,95 @@ card_filtering <- function(dataBase, district, baseline, pd, pp, n, acronym, sce
   
 }
 
+# test1 <- card_filtering(dataBase = dataFCards,
+#                         district = "THIES",
+#                         precipitation = FALSE,
+#                         pd = 1,
+#                         pp = 3.3,
+#                         n = 20,
+#                         acronym = "GIron",
+#                         year = )
+
 # Graph Filtering ----
-graph_filtering <- function(district, baseline, pd, pp, n, acronym){
+graph_filtering <- function(district, precipitation, pd, pp, n, acronym, unitsTable){
   
   path <- paste("data/Data_for_Kansas_", district, ".xlsx", sep = "")
   
-  if(baseline == TRUE){
-    sheet = "Baseline"
+  if(precipitation == TRUE){
+    
+    sheet = "precipitation"
     column1 = as.name(str_glue(acronym, " Base ", "1"))
     column2 = as.name(str_glue(acronym, " Base ", "5"))
+    
   }
   else{
+    
     sheet = paste0("Alt ", pd, " ", pp, " ", n, "kg")
     column1 = as.name(str_glue(acronym, " Alt ", "1"))
     column2 = as.name(str_glue(acronym, " Alt ", "5"))
-  }
-  
-  dataBase <- read_excel(path=path, sheet = sheet)
+    
+    dataBase <- read_excel(path=path, sheet = sheet)
 
-  values <- dataBase %>%
-    select(column1:column2)
-  
-  d1 <- data.frame(data=unlist(values, use.names = FALSE))
-  
-  multipliers <- c("GCal", "GIron", "GVitA")
-  
-  if(any(acronym %in% multipliers)){
+    values <- dataBase %>%
+      select(column1:column2)
     
-    d1$data <- d1$data*1000
+    d1 <- data.frame(data=unlist(values, use.names = FALSE))
+    
+    multipliers <- c("GCal", "GIron", "GVitA")
+    
+    if(any(acronym %in% multipliers)){
+      
+      d1$data <- d1$data*1000
+      
+    }
+    
+    Years <- rep(c("2016", "2017", "2018", "2019", "2020"),each=500)
+    d2 <- cbind(d1, Years)
+    
+    titulo <- toString(acronym)
+    unity <- unitsTable %>%
+      filter(Variable == titulo)
+    
+    unidade <- toString(unity$Unit)
+    
+    boxPlot <- ggplot(d2, aes(x=Years, y=data, color=Years))+
+      geom_boxplot(fill = "#1c5f7e",
+                   notch=TRUE)+
+      theme(
+        panel.background = element_rect(fill = "#1c5f7e",
+                                        colour = "#1c5f7e",
+                                        size = 0.2, linetype = "solid"),
+        plot.background = element_rect(fill = "#1c5f7e"),
+        
+        axis.title.y = element_text(size = 10,
+                                    colour = "white"),
+        axis.title.x = element_text(size = 10,
+                                    colour = "white"),
+        axis.text.y = element_text(size = 8,
+                                   face = "bold",
+                                   colour = "white"),
+        axis.text.x = element_text(size = 8,
+                                   face = "bold",
+                                   colour = "white"),
+        
+        legend.background = element_rect(fill = "#1c5f7e"),
+        legend.text = element_text(size = 10,
+                                   colour = "white"),
+        legend.title = element_text(colour = "white")
+        
+      )+
+      labs(y = unidade,
+           x = "Years")
     
   }
   
-  Scenarios <- rep(c("Alt 1", "Alt 2", "Alt 3", "Alt 4", "Alt 5"),each=500)
-  d2 <- cbind(d1, Scenarios)
-  
-  return(d2)
+  return(boxPlot)
   
 }
 
-dataBase <- graph_filtering(district = "THIES",
-                            baseline = FALSE,
-                            pd = 1,
-                            pp = 3.3,
-                            n = 20,
-                            acronym = "GIron")
+# test2 <- graph_filtering(district = "THIES",
+#                             precipitation = FALSE,
+#                             pd = 1,
+#                             pp = 3.3,
+#                             n = 20,
+#                             acronym = "GIron")
