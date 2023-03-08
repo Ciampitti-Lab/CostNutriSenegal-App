69a57735cd23a927a5f3aee878108af16c12aa57
Gustavo N. Santiago <104911112+GustavoSantiago113@users.noreply.github.com>
2023-03-08 16:57:32 -0600

    Feedbacks and responsive works

--- functions.R
+++ functions.R
@@ -2,8 +2,11 @@
 library(dplyr)
 library(readxl)
 library(stringr)
+library(ggplot2)
+library(scales)
 
 dataFCards <- read.csv("data/Alternative_Scenarios_Card.csv", sep=",", dec=".")
+units <- read.csv("data/Units.csv", sep = ",", dec = ".")
 
 # 1.0 Map Rendering ----
 map_rendering <- function(districtDB, district){
@@ -11,7 +14,9 @@ map_rendering <- function(districtDB, district){
   districtSelected <- districtDB %>%
     dplyr::filter(DISTRICT == district)
   
-  map <- leaflet() %>%
+  map <- leaflet(options = leafletOptions(zoomControl = FALSE,
+                                          minZoom = 6, maxZoom = 6,
+                                          dragging = FALSE)) %>%
     addTiles() %>%
     addPolygons( data = districtDB,
                  weight = 1,
@@ -79,18 +84,30 @@ card_filtering <- function(dataBase, district, precipitation, pd, pp, n, acronym
 #                         year = )
 
 # Graph Filtering ----
-graph_filtering <- function(district, precipitation, pd, pp, n, acronym, unitsTable){
+graph_filtering <- function(district, pd, pp, n, acronym, unitsTable){
   
   path <- paste("data/Data_for_Kansas_", district, ".xlsx", sep = "")
+  production <- read.csv("data/production.csv", sep=",", dec=".")
   
-  if(precipitation == TRUE){
+  if(acronym == "production"){
     
-    sheet = "precipitation"
-    column1 = as.name(str_glue(acronym, " Base ", "1"))
-    column2 = as.name(str_glue(acronym, " Base ", "5"))
+    pp <- as.integer(pp)
+    identification = paste0("pd", pd, "pp", pp, "n", n)
+    
+    districto <- toString(district)
+    
+    d2 <- production %>%
+      dplyr::filter(id == identification,
+                    district == districto) %>%
+      select(year, grain_yield) %>%
+      rename("Years" = "year",
+             "data" = "grain_yield")
+    
+    d2$Years <- as.character(d2$Years)
     
   }
-  else{
+  
+  else if(acronym != "production"){
     
     sheet = paste0("Alt ", pd, " ", pp, " ", n, "kg")
     column1 = as.name(str_glue(acronym, " Alt ", "1"))
@@ -111,53 +128,61 @@ graph_filtering <- function(district, precipitation, pd, pp, n, acronym, unitsTa
       
     }
     
+    else if(acronym == "NCFI"){
+      d1$data <- d1$data/(607.77)
+    }
+    
+    
     Years <- rep(c("2016", "2017", "2018", "2019", "2020"),each=500)
     d2 <- cbind(d1, Years)
     
-    titulo <- toString(acronym)
+  }
+  
+  titulo <- toString(acronym)
     unity <- unitsTable %>%
       filter(Variable == titulo)
     
-    unidade <- toString(unity$Unit)
-    
-    boxPlot <- ggplot(d2, aes(x=Years, y=data, color=Years))+
-      geom_boxplot(fill = "#1c5f7e",
-                   notch=TRUE)+
-      theme(
-        panel.background = element_rect(fill = "#1c5f7e",
-                                        colour = "#1c5f7e",
-                                        size = 0.2, linetype = "solid"),
-        plot.background = element_rect(fill = "#1c5f7e"),
-        
-        axis.title.y = element_text(size = 10,
-                                    colour = "white"),
-        axis.title.x = element_text(size = 10,
-                                    colour = "white"),
-        axis.text.y = element_text(size = 8,
-                                   face = "bold",
-                                   colour = "white"),
-        axis.text.x = element_text(size = 8,
-                                   face = "bold",
-                                   colour = "white"),
-        
-        legend.background = element_rect(fill = "#1c5f7e"),
-        legend.text = element_text(size = 10,
-                                   colour = "white"),
-        legend.title = element_text(colour = "white")
-        
-      )+
-      labs(y = unidade,
-           x = "Years")
-    
-  }
+  unidade <- toString(unity$Unit)
+  
+  boxPlot <- ggplot(d2, aes(x=Years, y=data, color=Years))+
+    geom_boxplot(fill = "white",
+                 notch=FALSE)+
+    theme(
+      panel.background = element_rect(fill = "#e3ecef",
+                                      colour = "#e3ecef",
+                                      size = 0.2, linetype = "solid"),
+      plot.background = element_rect(fill = "white"),
+      
+      axis.title.y = element_text(size = 10,
+                                  colour = "black"),
+      axis.title.x = element_text(size = 10,
+                                  colour = "black"),
+      axis.text.y = element_text(size = 8,
+                                 face = "bold",
+                                 colour = "black"),
+      axis.text.x = element_text(size = 8,
+                                 face = "bold",
+                                 colour = "black"),
+      
+      legend.background = element_rect(fill = "white"),
+      legend.text = element_text(size = 10,
+                                 colour = "black"),
+      legend.title = element_text(colour = "black")
+      
+    )+
+    labs(y = unidade,
+         x = "Years")+
+    scale_y_continuous(labels = comma)+
+    theme_bw()
   
   return(boxPlot)
   
 }
 
-# test2 <- graph_filtering(district = "THIES",
-#                             precipitation = FALSE,
-#                             pd = 1,
-#                             pp = 3.3,
-#                             n = 20,
-#                             acronym = "GIron")
+# test2 <- graph_filtering(district = "KAFFRINE",
+#                          pd = 1,
+#                          pp = 3.3,
+#                          n = 20,
+#                          acronym = "production",
+#                          unitsTable = units)
+# test2
