820cd4c033048f81bec2b509249406d659c7b9bd
Gustavo N. Santiago <104911112+GustavoSantiago113@users.noreply.github.com>
2023-03-14 16:38:23 -0500

    Version before Colorado meeting

--- functions.R
+++ functions.R
@@ -6,9 +6,10 @@ library(ggplot2)
 library(scales)
 
 # Data for testing -----
-dataFCards <- read.csv("data/Alternative_Scenarios_Card.csv", sep=",", dec=".")
-units <- read.csv("data/Units.csv", sep = ",", dec = ".")
-labels <- read.csv("~/K-State/CosNutSenegal-App/data/filteredPlantingDates.csv", sep = ",", dec = ".")
+# dataFCards <- read.csv("data/Alternative_Scenarios_Card.csv", sep=",", dec=".")
+# units <- read.csv("data/Units.csv", sep = ",", dec = ".")
+# labels <- read.csv("~/K-State/CosNutSenegal-App/data/filteredPlantingDates.csv", sep = ",", dec = ".")
+# precipitation <- read.csv("~/K-State/CosNutSenegal-App/data/precipitationData.csv", sep = ",", dec = ".")
 
 # 1.0 Map Rendering ----
 map_rendering <- function(districtDB, district){
@@ -50,7 +51,9 @@ map_rendering <- function(districtDB, district){
   
 }
 
-# 2.0 Label Filtering ----
+# 2.0 Inputs Filtering ----
+
+  ## 2.1 Select Input Filtering ----
 label_filter <- function(data, district){
   
   distrito <- toString(district)
@@ -65,29 +68,15 @@ label_filter <- function(data, district){
   
 }
 
-# distrito <- toString("KAOLACK")
-# 
-# dataFiltered <- labels %>%
-#   filter(district == distrito)
-# 
-# array <- c(dataFiltered$id)
-# names(array) <- c(dataFiltered$Label)
-# 
-# array
-
 # 3.0 Card Filtering ----
-card_filtering <- function(dataBase, district, precipitation, pd, pp, n, acronym, year){
+
+  ## 3.1 Alternative Management Based ----
+card_filtering <- function(dataBase, district, pd, pp, n, acronym, year){
   
   district <- toString(district)
   
-  if(precipitation == TRUE){
-    identification = "precipitation"
-    ## Add something after precipitation table arrive
-  }
-  else{
-    pp <- as.integer(pp)
-    identification = paste0("pd", pd, "pp", pp, "n", n)
-  }
+  pp <- as.integer(pp)
+  identification = paste0("pd", pd, "pp", pp, "n", n)
   
   dataBase <- dataBase %>%
     dplyr::filter(id == identification,
@@ -101,16 +90,45 @@ card_filtering <- function(dataBase, district, precipitation, pd, pp, n, acronym
   
 }
 
-# test1 <- card_filtering(dataBase = dataFCards,
-#                         district = "THIES",
-#                         precipitation = FALSE,
-#                         pd = 1,
-#                         pp = 3.3,
-#                         n = 20,
-#                         acronym = "GIron",
-#                         year = )
+  ## 3.2 Weather Based -----
+card_filtering_weather <- function(dataBase, district, pp, n, acronym, precipitationDB, precipitationValue){
+  
+  distrito <- toString(district)
+  
+  pp <- as.integer(pp)
+  
+  precipitationDB <- precipitationDB %>%
+    filter(district == distrito,
+           Precipitation == precipitationValue)
+  
+  ano <- precipitationDB$year
+  pd <- precipitationDB$planting_date_id
+  
+  yearDB <- data.frame(Ano = c(2016, 2017, 2018, 2019, 2020),
+                       choice = c(1, 2, 3, 4, 5))
+  
+  yearDB <- yearDB %>%
+    filter(Ano == ano)
+  
+  identification = paste0("pd", pd, "pp", pp, "n", n)
+  
+  dataBase <- dataBase %>%
+    dplyr::filter(id == identification,
+                  District == distrito)
+  
+  escolha <- yearDB$choice
+  
+  variable = as.name(str_glue(acronym, ".Alt.", escolha))
+  
+  value = dataBase[[variable]]
+  
+  return(value)
+  
+}
 
-# 4.0 Graph Filtering ----
+# 4.0 Graphs ----
+
+  ## 4.1 Alternative Management Based ----
 graph_filtering <- function(district, pd, pp, n, acronym, unitsTable){
   
   path <- paste("data/Data_for_Kansas_", district, ".xlsx", sep = "")
@@ -170,6 +188,7 @@ graph_filtering <- function(district, pd, pp, n, acronym, unitsTable){
       filter(Variable == titulo)
     
   unidade <- toString(unity$Unit)
+  variable <- toString(unity$Value)
   
   boxPlot <- ggplot(d2, aes(x=Years, y=data, color=Years))+
     geom_boxplot(fill = "white",
@@ -197,7 +216,7 @@ graph_filtering <- function(district, pd, pp, n, acronym, unitsTable){
       legend.title = element_text(colour = "black")
       
     )+
-    labs(y = unidade,
+    labs(y = str_glue(variable, "(", unidade, ")"),
          x = "Years")+
     scale_y_continuous(labels = comma)+
     theme_bw()
@@ -206,10 +225,149 @@ graph_filtering <- function(district, pd, pp, n, acronym, unitsTable){
   
 }
 
-# test2 <- graph_filtering(district = "KAFFRINE",
-#                          pd = 1,
-#                          pp = 3.3,
-#                          n = 20,
-#                          acronym = "production",
-#                          unitsTable = units)
-# test2
+  ## 4.2 Weather Based ----
+graph_filtering_WB <- function(district, pp, n, acronym, precipitationDB, precipitationValue, unitsTable){
+  
+  path <- paste("data/Data_for_Kansas_", district, ".xlsx", sep = "")
+  
+  distrito <- toString(district)
+  
+  precipitationDB <- precipitationDB %>%
+    filter(district == distrito,
+           Precipitation == precipitationValue)
+  
+  ano <- precipitationDB$year
+  pd <- precipitationDB$planting_date_id
+  
+  yearDB <- data.frame(Ano = c(2016, 2017, 2018, 2019, 2020),
+                       choice = c(1, 2, 3, 4, 5))
+  
+  yearDB <- yearDB %>%
+    filter(Ano == ano)
+  
+  escolha <- yearDB$choice
+  
+  sheet = paste0("Alt ", pd, " ", pp, " ", n, "kg")
+  column1 = as.name(str_glue(acronym, " Alt ", escolha))
+  
+  dataBase <- read_excel(path=path, sheet = sheet)
+  
+  d1 <- dataBase %>%
+    select(data = column1)
+  
+  multipliers <- c("GCal", "GIron", "GVitA")
+  
+  if(any(acronym %in% multipliers)){
+    
+    d1$data <- d1$data*1000
+    
+  }
+  
+  else if(acronym == "NCFI"){
+    d1$data <- d1$data/(607.77)
+  }
+  if(acronym == "GVitA"){
+    aprox <- 3
+  }else{
+    aprox <- 0
+  }
+
+  titulo <- toString(acronym)
+  unity <- unitsTable %>%
+    filter(Variable == titulo)
+  
+  unidade <- toString(unity$Unit)
+  variable <- toString(unity$Value)
+  
+  cdf_1 <- ecdf(d1$data)
+  value <- d1$data[which(cdf_1(d1$data) == 0.50)]
+  value1 <- d1$data[which(cdf_1(d1$data) == 0.90)]
+  
+  distribution_curve <- ggplot(d1, aes(x=data))+
+    stat_ecdf()+
+    geom_segment(x = 0,
+                 xend = value,
+                 y = 0.5,
+                 yend = 0.5,
+                 color = "#1c5f7e") + 
+    geom_segment(x = value,
+                 xend = value,
+                 y = 0,
+                 yend = 0.5,
+                 color = "#1c5f7e")+
+    geom_text(x = value1,
+              y = 0.25,
+              label = paste("50% of distribution is", str_glue(round(value, digits = aprox), " ", unidade), sep = "\n"),
+              color = "#1c5f7e")+
+    theme(
+      panel.background = element_rect(fill = "#e3ecef",
+                                      colour = "#e3ecef",
+                                      size = 0.2, linetype = "solid"),
+      plot.background = element_rect(fill = "white"),
+      
+      axis.title.y = element_text(size = 10,
+                                  colour = "black"),
+      axis.title.x = element_text(size = 10,
+                                  colour = "black"),
+      axis.text.y = element_text(size = 8,
+                                 face = "bold",
+                                 colour = "black"),
+      axis.text.x = element_text(size = 8,
+                                 face = "bold",
+                                 colour = "black"),
+      
+      legend.background = element_rect(fill = "white"),
+      legend.text = element_text(size = 10,
+                                 colour = "black"),
+      legend.title = element_text(colour = "black")
+      
+    )+
+    labs(y = "Distribution",
+         x = str_glue(variable, "(", unidade, ")"))+
+    scale_y_continuous(labels = comma)+
+    theme_bw()
+  
+  return(distribution_curve)
+  
+}
+  ## 4.3 Weather Based ----
+rain_density <- function(district, precipitationDB){
+  
+  distrito <- toString(district)
+  
+  precipitationDB <- precipitationDB %>%
+    filter(district == distrito)
+  
+  density_curve <- ggplot(precipitationDB, aes(x=Precipitation))+
+    geom_density()+
+    theme(
+      panel.background = element_rect(fill = "#e3ecef",
+                                      colour = "#e3ecef",
+                                      size = 0.2, linetype = "solid"),
+      plot.background = element_rect(fill = "white"),
+      
+      axis.title.y = element_text(size = 10,
+                                  colour = "black"),
+      axis.title.x = element_text(size = 10,
+                                  colour = "black"),
+      axis.text.y = element_text(size = 8,
+                                 face = "bold",
+                                 colour = "black"),
+      axis.text.x = element_text(size = 8,
+                                 face = "bold",
+                                 colour = "black"),
+      
+      legend.background = element_rect(fill = "white"),
+      legend.text = element_text(size = 10,
+                                 colour = "black"),
+      legend.title = element_text(colour = "black")
+      
+    )+
+    labs(y = "Density",
+         x = "Precipitation (mm)")+
+    scale_y_continuous(labels = comma)+
+    theme_bw()
+  
+  return(density_curve)
+  
+}
